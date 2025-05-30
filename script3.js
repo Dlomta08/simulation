de17a55eff82dd4bedb2e908c336cc806cd456f8
Bula7 <bulaurigiorgi1@gmail.com>
2025-05-30 18:54:27 +0400

    udidesi cvlilebebi

--- script3.js
+++
@@ -1,7 +0,0 @@
-function handleQuizClick(year, number, classNum) {
-    const yearFolder = year.replace("/", "-");        // e.g. "2023/24" → "2023-24"
-    const classFolder = `class-${classNum}`;          // e.g. 10 → "class-10"
-    const url = `${classFolder}/${yearFolder}/quiz-${number}.html`;
-    window.location.href = url;
-}
-  
