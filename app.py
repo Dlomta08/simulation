2b9cf6a4de1287461297c8c2a7290e4b3dd03953
Dlomta08 <davitlomtadze@mziuri.ge>
2025-07-02 22:47:58 +0400

    _

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def list_users():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0:8080", port=port)
+    app.run(host="0.0.0.0", port=port)
